58cd0630368bc2a7760cf96eb7dabb72d4acca92
sonyakostyukevich22 <43521365+sonyakostyukevich22@users.noreply.github.com>
2018-10-18 02:38:21 +0300

    Add files via upload

---
+++ integer3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+//4
+
+int main()
+{
+	int A;
+	std::cout << "A = " << std::endl;
+	std::cin >> A;
+	int B;
+	std::cout << "B = " << std::endl;
+	std::cin >> B;
+	std::cout << A / B << std::endl;
+	system("pause");
+	return 0;
+}
